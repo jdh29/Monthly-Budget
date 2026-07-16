346154973f0e89ae899664f386bc4d1c57a71414
jdh29 <jameshiggs29@gmail.com>
2026-07-16 19:00:00 +1000

    Update load.js

--- functions/api/load.js
+++ functions/api/load.js
@@ -3,7 +3,7 @@ export async function onRequestGet(context) {
   const url = new URL(request.url);
   const syncCode = url.searchParams.get('syncCode');
   if (!syncCode) return new Response('Missing sync code', { status: 400 });
-  const data = await env.APPS_KV.get(`budget:${syncCode}`);
+  const data = await env.APPS_KV.get(`budgetv4:${syncCode}`);
   if (!data) return new Response(JSON.stringify(null), {
     headers: { 'Content-Type': 'application/json' }
   });
