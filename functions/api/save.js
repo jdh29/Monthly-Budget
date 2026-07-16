cd7b023f109383f0c98d2c5370517366449c06a1
jdh29 <jameshiggs29@gmail.com>
2026-07-16 18:57:38 +1000

    Update save.js

--- functions/api/save.js
+++ functions/api/save.js
@@ -2,7 +2,7 @@ export async function onRequestPost(context) {
   const { request, env } = context;
   const { syncCode, data } = await request.json();
   if (!syncCode) return new Response('Missing sync code', { status: 400 });
-  await env.APPS_KV.put(`budget:${syncCode}`, JSON.stringify(data));
+  await env.APPS_KV.put(`budgetv4:${syncCode}`, JSON.stringify(data));
   return new Response(JSON.stringify({ ok: true }), {
     headers: { 'Content-Type': 'application/json' }
   });
