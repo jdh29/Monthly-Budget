f2ef21f0eaa9f5942924194ad9e40bc4717b8d26
jdh29 <jameshiggs29@gmail.com>
2026-07-17 20:47:18 +1000

    Delete functions/api/save.js

--- functions/api/save.js
+++
@@ -1,9 +0,0 @@
-export async function onRequestPost(context) {
-  const { request, env } = context;
-  const { syncCode, data } = await request.json();
-  if (!syncCode) return new Response('Missing sync code', { status: 400 });
-  await env.APPS_KV.put(`budgetv4:${syncCode}`, JSON.stringify(data));
-  return new Response(JSON.stringify({ ok: true }), {
-    headers: { 'Content-Type': 'application/json' }
-  });
-}
