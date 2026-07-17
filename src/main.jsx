2d01843c30c8c85a8b79a4929479caabbc462033
jdh29 <jameshiggs29@gmail.com>
2026-07-17 22:33:27 +1000

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,35 @@
+import React from 'react'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { error: null }
+  }
+  static getDerivedStateFromError(error) {
+    return { error }
+  }
+  render() {
+    if (this.state.error) {
+      return (
+        <div style={{ color: 'white', background: '#111', padding: 20, fontFamily: 'sans-serif', whiteSpace: 'pre-wrap' }}>
+          Something crashed:{'\n\n'}
+          {this.state.error.message}
+          {'\n\n'}
+          {this.state.error.stack}
+        </div>
+      )
+    }
+    return this.props.children
+  }
+}
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ErrorBoundary>
+      <App />
+    </ErrorBoundary>
   </StrictMode>,
 )
